9177f066abdc064d388250a976fa0d3d0c6af4b8
Jacob Chvatal <jake@isnt.online>
2022-12-18 08:12:20 -0500

    avoid metal conflict?

--- src/humble_deck/main.clj
+++ src/humble_deck/main.clj
@@ -1,32 +1,27 @@
 (ns humble-deck.main
   (:require
-    [humble-deck.common :as common]
-    [humble-deck.controls :as controls]
-    [humble-deck.overview :as overview]
-    [humble-deck.slides :as slides]
-    [humble-deck.speaker :as speaker]
-    [humble-deck.state :as state]
-    [io.github.humbleui.app :as app]
-    [io.github.humbleui.debug :as debug]
-    [io.github.humbleui.ui :as ui])
+   [humble-deck.common :as common]
+   [humble-deck.controls :as controls]
+   [humble-deck.overview :as overview]
+   [humble-deck.slides :as slides]
+   [humble-deck.speaker :as speaker]
+   [humble-deck.state :as state]
+   [io.github.humbleui.app :as app]
+   [io.github.humbleui.debug :as debug]
+   [io.github.humbleui.ui :as ui])
   (:import
-    [io.github.humbleui.skija ColorSpace]
-<<<<<<< HEAD
-    [io.github.humbleui.jwm Window]))
-=======
-    [io.github.humbleui.jwm Window]
-    [io.github.humbleui.jwm.skija LayerMetalSkija LayerGLSkija]))
->>>>>>> 3fd5359 (no instacrash)
+   [io.github.humbleui.skija ColorSpace]
+   [io.github.humbleui.jwm Window]))
 
 (def app
   (common/with-context
     (controls/key-listener
-      (ui/stack
-        (ui/dynamic _ [{:keys [mode]} @state/*state]
-          (case mode
-            :overview overview/overview
-            :present  common/slide))
-        controls/controls))))
+     (ui/stack
+      (ui/dynamic _ [{:keys [mode]} @state/*state]
+                  (case mode
+                    :overview overview/overview
+                    :present  common/slide))
+      controls/controls))))
 
 (reset! state/*app app)
 
@@ -35,23 +30,17 @@
 (reset! state/*speaker-app speaker/speaker-app)
 
 (add-watch state/*state ::redraw
-  (fn [_ _ old new]
-    (when (not= old new)
-      (common/redraw))))
+           (fn [_ _ old new]
+             (when (not= old new)
+               (common/redraw))))
 
 (defn -main [& _args]
   (ui/start-app!
-    (deliver state/*window
-      (ui/window
-        {:title    "Humble Deck"
-         :mac-icon "resources/icon.icns"
-         :bg-color 0xFFFFFFFF}
-        state/*app)))
-<<<<<<< HEAD
-  (when (= :macos app/platform)
-    (set! (.-_colorSpace (.getLayer ^Window @state/*window)) (ColorSpace/getDisplayP3)))
-=======
+   (deliver state/*window
+            (ui/window
+             {:title    "Humble Deck"
+              :mac-icon "resources/icon.icns"
+              :bg-color 0xFFFFFFFF}
+             state/*app)))
   (set! (.-_colorSpace ^LayerGLSkija (.getLayer ^Window @state/*window)) (ColorSpace/getDisplayP3))
->>>>>>> 3fd5359 (no instacrash)
-  ; (reset! debug/*enabled? true)
   (common/redraw))
